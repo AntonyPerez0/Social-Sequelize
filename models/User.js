dc39caf74f28b3f346185a4d9ad3d42ec422ecfd
Antony Perez <antonyperez0@yahoo.com>
2024-11-06 09:44:14 -0800

    did the user file

--- models/User.js
+++ models/User.js
@@ -1,3 +1,15 @@
-let User;
+const { DataTypes } = require("sequelize");
+const { db } = require("../db/connection");
+
+const User = db.define("User", {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
 
 module.exports = User;
