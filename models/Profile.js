d0df39ce7db8b258bb48a22d027e6b39c6f40940
Antony Perez <antonyperez0@yahoo.com>
2024-11-06 09:59:42 -0800

    pushing the Profile.js file

--- models/Profile.js
+++ models/Profile.js
@@ -1,3 +1,16 @@
-let Profile;
+const { DataTypes } = require("sequelize");
+const { db } = require("../db/connection");
+
+const Profile = db.define("Profile", {
+  bio: {
+    type: DataTypes.STRING,
+  },
+  profilePicture: {
+    type: DataTypes.STRING,
+  },
+  birthday: {
+    type: DataTypes.STRING,
+  },
+});
 
 module.exports = Profile;
