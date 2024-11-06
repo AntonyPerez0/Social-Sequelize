ec71db46d3a2c36c0f7bdc19a5e8baf5aabde27a
Antony Perez <antonyperez0@yahoo.com>
2024-11-06 10:28:13 -0800

    finsihed up Like.js

--- models/Like.js
+++ models/Like.js
@@ -1,3 +1,13 @@
-let Like;
+const { DataTypes } = require("sequelize");
+const { db } = require("../db/connection");
+
+const Like = db.define("Like", {
+  reactionType: {
+    type: DataTypes.STRING,
+  },
+  createdAt: {
+    type: DataTypes.STRING,
+  },
+});
 
 module.exports = Like;
