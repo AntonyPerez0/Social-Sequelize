2854c3910f487d7df0002ab800ab6e2a73d9d9d5
Antony Perez <antonyperez0@yahoo.com>
2024-11-06 10:27:07 -0800

    pushing commnet.js

--- models/Comment.js
+++ models/Comment.js
@@ -1,3 +1,13 @@
-let Comment;
+const { DataTypes } = require("sequelize");
+const { db } = require("../db/connection");
+
+const Comment = db.define("Comment", {
+  body: {
+    type: DataTypes.STRING,
+  },
+  createdAt: {
+    type: DataTypes.STRING,
+  },
+});
 
 module.exports = Comment;
