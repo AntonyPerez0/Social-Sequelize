174038e30e3923dc17911c8257369fe7d65b89e4
Antony Perez <antonyperez0@yahoo.com>
2024-11-06 10:11:11 -0800

    pushing post.js file

--- models/Post.js
+++ models/Post.js
@@ -1,3 +1,16 @@
-let Post;
+const { DataTypes } = require("sequelize");
+const { db } = require("../db/connection");
+
+const Post = db.define("Post", {
+  title: {
+    type: DataTypes.STRING,
+  },
+  body: {
+    type: DataTypes.STRING,
+  },
+  createdAt: {
+    type: DataTypes.STRING,
+  },
+});
 
 module.exports = Post;
